98ee3402e943a2f3dc08f7e58c99a307691ba464
yarik.s <yarik.savelev.04@mail.ru>
2026-07-02 17:30:21 +0300

    upd

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,30 +1,29 @@
 "use client";
 
-import { dataStyle } from "@/resources/content";
-import { AppToaster } from "@/shared/components/Toaster";
 import {
   ChartMode,
   ChartVariant,
   DataThemeProvider,
-  iconLibrary,
   IconProvider,
+  iconLibrary,
   LayoutProvider,
 } from "@once-ui-system/core";
+import { dataStyle } from "@/resources/content";
+import { AppToaster } from "@/shared/components/Toaster";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <LayoutProvider>
-        <DataThemeProvider
-          variant={dataStyle.variant as ChartVariant}
-          mode={'categorical' as ChartMode}
-          height={400}
-        >
-            <IconProvider icons={iconLibrary}>{children}
-              <AppToaster />
-
-            </IconProvider>
-        </DataThemeProvider>
-
+      <DataThemeProvider
+        variant={dataStyle.variant as ChartVariant}
+        mode={"categorical" as ChartMode}
+        height={400}
+      >
+        <IconProvider icons={iconLibrary}>
+          {children}
+          <AppToaster />
+        </IconProvider>
+      </DataThemeProvider>
     </LayoutProvider>
   );
 }
